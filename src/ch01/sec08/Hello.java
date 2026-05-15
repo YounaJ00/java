5db9525b46d42142a2f4f98011536f22fd24a1ed
Youna Joo <you.na.joo0@gmail.com>
2026-05-15 14:45:55 +0900

    feat: Hello.java

---
+++ src/ch01/sec08/Hello.java
@@ -0,0 +1,22 @@
+package ch01.sec08;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요?!"); // cmd + F11
+		System.out.println("2");
+
+		test();
+	}
+
+	/**
+	 * 이 메서드는 테스트입니다.
+	 * 
+	 * @return name
+	 */
+	public static String test() {
+		return "";
+	}
+
+}
